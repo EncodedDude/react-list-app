847000a111af55956dd5a52378372b21358cd69e
unknown <dima35zotov@mail.ru>
2025-12-30 11:38:35 +0300

    added disabled state and handler for button and realize adding values to list

--- list-app/src/App.jsx
+++ list-app/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 	const [list, setList] = useState([]);
 	const [error, setError] = useState("");
 
+	let isValueValid = value.length < 3 ? false : true;
+
 	const onInputButtonClick = () => {
 		const promptValue = prompt("Введите значение");
 		if (promptValue.length < 3) {
@@ -14,6 +16,33 @@ function App() {
 			setValue(promptValue);
 			setError("");
 		}
+	};
+
+	const onAddButtonClick = () => {
+		if (isValueValid) {
+			setList((updatedList) => [
+				...updatedList,
+				{ id: Date.now(), value, date: formatedDate(Date.now()) },
+			]);
+			setValue("");
+			setError("");
+		}
+	};
+
+	const addZero = (num) => {
+		return String(num).length === 1 ? `0${num}` : num;
+	};
+
+	const formatedDate = (ms) => {
+		const dateForFormat = new Date(ms);
+		const year = addZero(dateForFormat.getFullYear());
+		const month = addZero(dateForFormat.getMonth()) + 1;
+		const numberDate = addZero(dateForFormat.getDate());
+		const hour = addZero(dateForFormat.getHours());
+		const minute = addZero(dateForFormat.getMinutes());
+		const second = addZero(dateForFormat.getSeconds());
+
+		return `${numberDate}.${month}.${year} ${hour}:${minute}:${second}`;
 	};
 
 	return (
@@ -33,18 +62,29 @@ function App() {
 					>
 						Ввести новое
 					</button>
-					<button className={styles.button} disabled>
+					<button
+						className={styles.button}
+						disabled={!isValueValid}
+						onClick={onAddButtonClick}
+					>
 						Добавить в список
 					</button>
 				</div>
 				<div className={styles["list-container"]}>
 					<h2 className={styles["list-heading"]}>Список:</h2>
-					<p className={styles["no-margin-left"]}>
-						Нет добавленных элементов
-					</p>
-					<ul className={styles.list}>
-						<li className={styles["list-item"]}>Первый элемент</li>
-					</ul>
+					{list.length === 0 ? (
+						<p className={styles["no-margin-left"]}>
+							Нет добавленных элементов
+						</p>
+					) : (
+						<ul className={styles.list}>
+							{list.map(({ id, value, date }) => (
+								<li className={styles["list-item"]} key={id}>
+									{value} - {date}
+								</li>
+							))}
+						</ul>
+					)}
 				</div>
 			</div>
 		</>
